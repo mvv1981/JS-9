055a61a96cee06537705d94911e3478edae70914
max-barabash <48650035+max-barabash@users.noreply.github.com>
2019-04-20 17:20:14 +0400

    Update module

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
+/**
+ * Барабаш Максим Сергеевич
+ */
 module.exports = {
-
+    eventList: [],
     /**
      * @param {String} event
      * @param {Object} subscriber
      * @param {Function} handler
      */
     on: function (event, subscriber, handler) {
-
+        this.eventList.push({
+            event,
+            subscriber,
+            handler
+        });
+        return this
     },
 
     /**
@@ -14,13 +22,23 @@ module.exports = {
      * @param {Object} subscriber
      */
     off: function (event, subscriber) {
-
+        for (let i = this.eventList.length - 1; i > 0; i--) {
+            if ((this.eventList[i].event === event)&&(this.eventList[i].subscriber === subscriber)) {
+                this.eventList.splice(i, 1);
+            }
+        }
+        return this
     },
 
     /**
      * @param {String} event
      */
     emit: function (event) {
-
+        for (let i = 0; i < this.eventList.length; i++) {
+            if(this.eventList[i].event === event) {
+                this.eventList[i].handler.call(this.eventList[i].subscriber)
+            }
+        }
+        return this
     }
 };
